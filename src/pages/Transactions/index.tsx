67112b8f91870459e113d8694bd6de37d145749f
Matheus Eufrasio <matheuseufrasio2@gmail.com>
2022-10-12 05:36:27 -0300

    create Header

---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,12 @@
+import { Header } from '../../components/Header';
+import {
+  TransactionsContainer,
+} from './styles';
+
+export function Transactions(){
+  return (
+    <TransactionsContainer>
+      <Header  />
+    </TransactionsContainer>
+  );
+}
