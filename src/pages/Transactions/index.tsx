9667a1f4b48f01b33d6e5218bbcdeccfb3c8f2c3
Matheus Eufrasio <matheuseufrasio2@gmail.com>
2022-10-13 12:22:24 -0300

    create search form

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,14 +1,41 @@
 import { Header } from '../../components/Header';
 import { Summary } from '../../components/Summary';
+import { SearchForm } from './components/SearchForm';
 import {
-  TransactionsContainer,
+  PriceHighlight,
+  TransactionsContainer, TransactionsTable,
 } from './styles';
 
 export function Transactions(){
   return (
-    <TransactionsContainer>
+    <div>
       <Header  />
       <Summary />
-    </TransactionsContainer>
+
+
+      <TransactionsContainer>
+        <SearchForm />
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant='income'>R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Macbook</td>
+              <td>
+                <PriceHighlight variant='outcome'>-R$ 9.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>19/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+    </div>
   );
 }
